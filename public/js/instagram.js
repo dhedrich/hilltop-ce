58b2bbd32acc6d2ce5d1c08f77762f26716991ed
dhedrich <donovanhedrich@gmail.com>
2018-09-26 12:46:24 -0700

    instagram scraper working, added new cover photo

--- public/js/instagram.js
+++ public/js/instagram.js
@@ -1,2 +1,9 @@
-let postPromise = getPosts('hotpinksunrise')       
-postPromise.then(posts => console.log(posts))
+// get instagram posts
+$.ajax({
+    url: "/insta",
+    method: "GET",
+}).done(data => {
+    renderPosts(data)
+}).error(error => {
+    console.log("Error: posts not found")
+})
